cce93074ec1900e44a0dd4173e23a1d56dfa8bd5
taeate <yes3144@nate.com>
2022-01-17 11:37:48 +0900

    채팅 분리중-1

--- chat/urls.py
+++ chat/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('detail/<int:room_id>/', views.room_detail, name="detail"),
     path('detail/<int:room_id>/accessServer/', views.accessServer, name="accessServer"),
     path('detail/messages/write', views.message_write, name='message_write'),
-    path('detail/messages/', views.chat, name='chat'),
+    path('detail/messages/<int:room_id>', views.chat, name='chat'),
 ]
