ff9e35a448d6c8acb70a259048e657b787806ee4
kimchaemin000617 <dktjdej25@gmail.com>
2022-01-24 17:39:19 +0900

    user모델 email 삭제, nickname 추가, 회원가입 페이지 버튼 픽스

--- accounts/form.py
+++ accounts/form.py
@@ -3,8 +3,6 @@ from accounts.models import User
 
 
 class UserForm(forms.ModelForm):
-    email = forms.EmailField(label="이메일")
-
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "password1", "password2", "nickname")
--- accounts/models.py
+++ accounts/models.py
@@ -7,4 +7,5 @@ class User(AbstractUser):
     friends = models.ManyToManyField('self')
     password1 = models.CharField(max_length=50)
     password2 = models.CharField(max_length=50)
-
+    nickname = models.CharField(max_length=50, unique=True)
+    email = False
