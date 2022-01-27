20f4c5356a308025a423d6e6aaa6930ad4c958a2
taeate <yes3144@nate.com>
2022-01-27 09:39:06 +0900

    tailwind 적용

--- base/settings.py
+++ base/settings.py
@@ -11,6 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base/static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 AUTH_USER_MODEL = 'accounts.User'
 # Default primary key field type
--- base/urls.py
+++ base/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -9,4 +11,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', auth_views.LoginView.as_view(template_name='accounts_login.html'), name='accounts'),
     path('chat/', include('chat.urls')),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
