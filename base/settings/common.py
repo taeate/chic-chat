daa263e235179df29cec88610ed47fd625b0661f
taeate <yes3144@nate.com>
2022-02-24 22:30:05 +0900

    jenkins test

--- base/settings/common.py
+++ base/settings/common.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'base.custom.TimeChecker',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
